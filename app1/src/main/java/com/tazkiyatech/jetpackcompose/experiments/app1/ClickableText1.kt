6da8158958c886bcddef31a2df08c9c5c2693117
Adil Hussain <>
2023-11-17 12:52:54 +0000

    (app1) Removed `Greeting` and added `ClickableText1`.

---
+++ app1/src/main/java/com/tazkiyatech/jetpackcompose/experiments/app1/ClickableText1.kt
@@ -0,0 +1,73 @@
+package com.tazkiyatech.jetpackcompose.experiments.app1
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withAnnotation
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.res.TypedArrayUtils.getText
+import com.tazkiyatech.jetpackcompose.experiments.app1.ui.theme.AppTheme
+
+@Composable
+fun ClickableText1() {
+    val context = LocalContext.current
+
+    ClickableText(
+        text = getText(),
+        style = MaterialTheme.typography.bodyLarge,
+        onClick = { onClick(context, it) }
+    )
+}
+
+@OptIn(ExperimentalTextApi::class)
+private fun getText(): AnnotatedString {
+    return buildAnnotatedString {
+        append("You can tap ")
+        withStyle(SpanStyle(color = Color.Blue, textDecoration = TextDecoration.Underline)) {
+            withAnnotation("Link", "Link 1") {
+                append("here")
+            }
+        }
+        append(" or ")
+        withStyle(SpanStyle(color = Color.Blue, textDecoration = TextDecoration.Underline)) {
+            withAnnotation("Link", "Link 2") {
+                append("here")
+            }
+        }
+        append(" and you'll see a different popup depending on where you tap.")
+    }
+}
+
+private fun onClick(context: Context, offset: Int) {
+    Log.d("ClickableText1", "onClick(offset = $offset)")
+
+    val annotations = getText().getStringAnnotations(
+        tag = "Link",
+        start = offset,
+        end = offset
+    )
+
+    val annotation = annotations.firstOrNull() ?: return
+
+    Toast.makeText(context, annotation.item, Toast.LENGTH_SHORT).show()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ClickableText1Preview() {
+    AppTheme {
+        ClickableText1()
+    }
+}
--- app1/src/main/java/com/tazkiyatech/jetpackcompose/experiments/app1/MainActivity.kt
+++ app1/src/main/java/com/tazkiyatech/jetpackcompose/experiments/app1/MainActivity.kt
@@ -3,41 +3,33 @@ package com.tazkiyatech.jetpackcompose.experiments.app1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.dp
 import com.tazkiyatech.jetpackcompose.experiments.app1.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             AppTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        ClickableText1()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
     }
 }
