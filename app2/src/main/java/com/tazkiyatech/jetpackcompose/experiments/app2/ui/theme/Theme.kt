45183e066cbaa0b0c1cc6750ca1a2876800b073c
Adil Hussain <>
2023-11-24 10:40:07 +0000

    (app2) Applied some auto-formatting to the `Theme.kt` file.

--- app2/src/main/java/com/tazkiyatech/jetpackcompose/experiments/app2/ui/theme/Theme.kt
+++ app2/src/main/java/com/tazkiyatech/jetpackcompose/experiments/app2/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -24,13 +25,13 @@ import com.tazkiyatech.jetpackcompose.experiments.app2.ui.theme.PurpleGrey80
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -46,7 +47,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
