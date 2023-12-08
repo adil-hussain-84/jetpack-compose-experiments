4dd64abb2f3d313e6caacd2d08cc6cd6e25cea11
Adil Hussain <>
2023-12-08 09:01:46 +0000

    Renamed "app2" to "app3" and started using a version catalog in the Gradle files.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,11 +8,19 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
     }
+
+    versionCatalogs {
+        create("libs") {
+            library("activity-compose", "androidx.activity:activity-compose:1.8.1")
+            library("compose-bom", "androidx.compose:compose-bom:2023.10.01")
+        }
+    }
 }
 
 include(":app1")
-include(":app2")
+include(":app3")
