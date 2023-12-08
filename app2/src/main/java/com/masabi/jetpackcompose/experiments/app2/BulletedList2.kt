c4431bbb0f3828ee1eee001013e38fe74eda3a0f
Adil Hussain <>
2023-12-08 13:07:35 +0000

    (app2) Added `BulletedList2`.

--- app2/src/main/java/com/masabi/jetpackcompose/experiments/app2/BulletedList2.kt
+++ app2/src/main/java/com/masabi/jetpackcompose/experiments/app2/BulletedList2.kt
@@ -2,6 +2,7 @@ package com.masabi.jetpackcompose.experiments.app2
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,25 +12,31 @@ import androidx.compose.ui.unit.dp
 import com.masabi.jetpackcompose.experiments.app2.ui.theme.AppTheme
 
 @Composable
-fun BulletedList1(modifier: Modifier = Modifier) {
+fun BulletedList2(modifier: Modifier = Modifier) {
     Column(verticalArrangement = Arrangement.spacedBy(3.dp)) {
-        BulletedText(text = "This is a long piece of text which will span over to a second line but won't indent to the right of the bullet point.")
-        BulletedText(text = "This is another long piece of text which will span over to a second line and won't indent to the right of the bullet point.")
+        BulletedText(text = "This is a long piece of text which will span over to a second line and will indent to the right of the bullet point.")
+        BulletedText(text = "This is another long piece of text which will span over to a second line and will indent to the right of the bullet point.")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun BulletedList1Preview() {
+fun BulletedList2Preview() {
     AppTheme {
-        BulletedList1()
+        BulletedList2()
     }
 }
 
 @Composable
 private fun BulletedText(text: String) {
-    Text(
-        text = "• $text",
-        style = MaterialTheme.typography.bodyLarge
-    )
+    Row() {
+        Text(
+            text = "• ",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
 }
--- app2/src/main/java/com/masabi/jetpackcompose/experiments/app2/MainView.kt
+++ app2/src/main/java/com/masabi/jetpackcompose/experiments/app2/MainView.kt
@@ -3,6 +3,7 @@ package com.masabi.jetpackcompose.experiments.app2
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +17,9 @@ fun MainView(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
         Text("Solution 1", style = MaterialTheme.typography.titleLarge)
         BulletedList1()
+        Divider(thickness = 1.dp)
+        Text("Solution 2", style = MaterialTheme.typography.titleLarge)
+        BulletedList2()
     }
 }
 
