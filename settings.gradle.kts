26a9545612d84653fb9ec725911ac0eba9f3f59f
Adil Hussain <>
2024-01-12 14:20:35 +0000

    Upgraded plugins and libraries.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,13 +16,13 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("compilers") {
-            version("compose", "1.5.6")
+            version("compose", "1.5.8")
         }
         create("libs") {
-            library("activity-compose", "androidx.activity:activity-compose:1.8.1")
+            library("activity-compose", "androidx.activity:activity-compose:1.8.2")
             library("compose-bom", "androidx.compose:compose-bom:2023.10.01")
             library("core-ktx", "androidx.core:core-ktx:1.12.0")
-            library("lifecycle-runtime-ktx", "androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+            library("lifecycle-runtime-ktx", "androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
         }
     }
 }
