322b561319fa0d5dfef167638bb258aed1f8f634
Adil Hussain <adilson05uk@gmail.com>
2024-02-02 11:29:10 +0000

    (app3) Added count to "MainView".

--- app3/src/main/java/com/tazkiyatech/compose/app3/MainActivity.kt
+++ app3/src/main/java/com/tazkiyatech/compose/app3/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.tazkiyatech.compose.app3.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MainView("Android")
+                    MainView(modifier = Modifier.padding(16.dp))
                 }
             }
         }
--- app3/src/main/java/com/tazkiyatech/compose/app3/MainView.kt
+++ app3/src/main/java/com/tazkiyatech/compose/app3/MainView.kt
@@ -1,23 +1,40 @@
 package com.tazkiyatech.compose.app3
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tazkiyatech.compose.app3.ui.theme.AppTheme
 
 @Composable
-fun MainView(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainView(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Text(text = "Count is $count")
+        Button(onClick = { count++ }) {
+            Text(text = "Increment Count")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MainViewPreview() {
     AppTheme {
-        MainView("Android")
+        MainView()
     }
 }
