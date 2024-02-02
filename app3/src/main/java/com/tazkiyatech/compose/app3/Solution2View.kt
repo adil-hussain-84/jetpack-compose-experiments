f9b7fcedd9d1ed773718b11d4942641d6f56bb7a
Adil Hussain <adilson05uk@gmail.com>
2024-02-02 11:44:08 +0000

    (app3) Added `Solution1View` and `Solution2View`.

--- app3/src/main/java/com/tazkiyatech/compose/app3/Solution2View.kt
+++ app3/src/main/java/com/tazkiyatech/compose/app3/Solution2View.kt
@@ -2,34 +2,39 @@ package com.tazkiyatech.compose.app3
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun MainView(modifier: Modifier = Modifier) {
+fun Solution2View(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
-        Text(text = "Solution 1", style = MaterialTheme.typography.titleLarge)
-        Solution1View()
-        Divider(thickness = 1.dp)
-        Text(text = "Solution 2", style = MaterialTheme.typography.titleLarge)
-        Solution2View()
+        Text(text = "Count is $count", style = MaterialTheme.typography.bodyLarge)
+        Button(onClick = { count++ }) {
+            Text(text = "Increment Count")
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MainViewPreview() {
+fun Solution2ViewPreview() {
     AppTheme {
-        MainView()
+        Solution2View()
     }
 }
