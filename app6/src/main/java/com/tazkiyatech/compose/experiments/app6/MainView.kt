95f24a546a0c81d4106c596de1cc0a16fa218fe3
Adil Hussain <adilson05uk@gmail.com>
2025-07-11 16:35:34 +0100

    (app6) Change the introductory text slightly.

--- app6/src/main/java/com/tazkiyatech/compose/experiments/app6/MainView.kt
+++ app6/src/main/java/com/tazkiyatech/compose/experiments/app6/MainView.kt
@@ -7,23 +7,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tazkiyatech.compose.experiments.app6.theme.AppTheme
 
 @Composable
 fun MainView(modifier: Modifier = Modifier) {
-    val introductoryMessage =
-        "Run this app with TalkBack enabled on your device. If your device has its primary language set to a language that is not Arabic, you will notice that TalkBack does not read out the Arabic text."
-
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
-            text = introductoryMessage,
-            style = MaterialTheme.typography.bodyLarge,
+            text = stringResource(R.string.introductory_text),
+            textAlign = TextAlign.Justify,
+            style = MaterialTheme.typography.bodyMedium,
         )
         Demo1View()
         Demo2View()
